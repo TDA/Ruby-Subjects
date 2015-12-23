5cdffada1a2dca4a96cf1e0c4a22a89a042aed9f
Sai Pc <schand31@asu.edu>
2015-12-23 23:18:27 +0530

    implementing our own > operator

--- subjects.rb
+++ subjects.rb
@@ -1,5 +1,12 @@
 class Subjects
   attr_accessor :name
+  # any name starting with a Caps is considered
+  # constant by ruby
+  # By convention, all caps is preferred
+  SUBJECT_POINTS = {
+      "Software Security" => 5,
+      "Semantic Web Mining" => 4
+  }
 
   def initialize(name)
     self.name = name
@@ -7,7 +14,7 @@ class Subjects
 
   # overriding comparisons
   def >(other)
-
+    SUBJECT_POINTS[self.name] > SUBJECT_POINTS[other.name]
   end
 
 end
